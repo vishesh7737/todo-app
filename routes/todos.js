93a2a10f1cfc3158ba23935975f9d6773f085402
vishesh7737 <visheshjayaswal8910@gmail.com>
2026-02-21 21:01:11 +0530

    Fix import case for Todo model

--- routes/todos.js
+++ routes/todos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Todo = require('../models/todo');
+const Todo = require('../models/Todo');
 
 // GET all todos
 router.get('/', async (req, res) => {
